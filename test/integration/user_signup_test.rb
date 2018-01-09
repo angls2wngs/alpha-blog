b3f95eb671edabac3843e59369f9c04d3a0f4059
robynjtucker <robynjtucker@gmail.com>
2018-01-09 03:12:23 +0000

    new user signup test

---
+++ test/integration/user_signup_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class UserSignupTest < ActionDispatch::IntegrationTest
+  
+  test "new user signup" do
+    get signup_path  
+    assert_template 'users/new'
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: {name: "testing", email: "testing@example.com", password: "password" }
+    end
+    assert_template 'users/show'
+    assert_match "testing", response.body
+  end
+end
+
